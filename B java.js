e37ef4475ca5a75ec92be48c8aa782b3bcd52200
mariummshafiq <mariumshafiq959@gmail.com>
2025-01-24 13:23:47 +0500

    Add files via upload

---
+++ B java.js
@@ -0,0 +1,33 @@
+function welcomeMessage() {
+    alert("Welcome to My Landing Page!");
+}
+
+function personalizedGreeting() {
+    let name = prompt("Please enter your name:");
+    if (name != null) {
+        alert(`Hello, ${name}!`);
+    }
+}
+function whatwasyourfavoritememoryfromthepastyear() {
+    let name = prompt("what was your favorite memory from the past year:");
+    if (name != null) {
+        alert(`what was your favorite memory from the past year!, ${name}!`);
+    }
+}
+
+function howdidyour365daysgo() {
+    let name = prompt("how did your 365 days go:");
+    if (name != null) {
+        alert(`how did your 365 days go!, ${name}!`);
+    }
+}
+function yournewyearsresolutions() {
+    let name = prompt("your new year resolutions:");
+    if (name != null) {
+        alert(`your new year resolutions!, ${name}!`);
+    }
+}
+
+function userInfo() {
+    alert("My name is MARIUM SHAFIQ. I AM YOUR CLASS FALLOW.");
+}
